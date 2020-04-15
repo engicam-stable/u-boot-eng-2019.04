8ea284c4bce5f371b64a668505f8777e6fcd6b2b
Mirko Ardinghi <mirko.ardinghi@engicam.com>
2020-04-15 21:33:14 +0200

    icore mx6dl1.5: removed framebuffer

--- include/configs/icorem6_common.h
+++ include/configs/icorem6_common.h
@@ -179,17 +179,6 @@
   #define CONFIG_SYS_I2C_SPEED		  100000
 #endif
 
-/* Framebuffer */
-#define CONFIG_VIDEO_IPUV3
-#define CONFIG_VIDEO_BMP_RLE8
-#define CONFIG_SPLASH_SCREEN
-#define CONFIG_SPLASH_SCREEN_ALIGN
-#define CONFIG_BMP_16BPP
-#define CONFIG_VIDEO_LOGO
-#define CONFIG_VIDEO_BMP_LOGO
-#define CONFIG_IMX_HDMI
-#define CONFIG_IMX_VIDEO_SKIP
-
 #if defined(CONFIG_ANDROID_SUPPORT)
   #include "mx6sabreandroid_common.h"
 #else
